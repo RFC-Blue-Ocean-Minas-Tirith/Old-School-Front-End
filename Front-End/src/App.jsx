f2e7f80f8e0640a6acc1c7770af406f8136402dd
Zachariah1618 <zvallow@gmail.com>
2022-09-30 15:25:37 -0500

    cleaned up file structure for use of project

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,53 +1,10 @@
 /* eslint-disable no-shadow */
-import { useState } from 'react';
-import { Button } from 'react-bootstrap';
-import reactLogo from './assets/react.svg';
-import './App.css';
+// import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((count) => count + 1)}>
-          count is
-          {count}
-        </button>
-        <p>
-          Edit
-          <code>src/App.jsx</code>
-          and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Button variant="primary">Primary</Button>
-      {' '}
-      <Button variant="secondary">Secondary</Button>
-      {' '}
-      <Button variant="success">Success</Button>
-      {' '}
-      <Button variant="warning">Warning</Button>
-      {' '}
-      <Button variant="danger">Danger</Button>
-      {' '}
-      <Button variant="info">Info</Button>
-      {' '}
-      <Button variant="light">Light</Button>
-      {' '}
-      <Button variant="dark">Dark</Button>
-      <Button variant="link">Link</Button>
+      Hello World
     </div>
   );
 }
