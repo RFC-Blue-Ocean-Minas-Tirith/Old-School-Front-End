7de46416ae3dc476de1b87256a09b87734f05af1
Vicki Wong <vicki.wong85@gmail.com>
2022-09-30 22:09:52 -0500

    added all modal component files and created simple modal structure for a blog post

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,10 +1,14 @@
 /* eslint-disable no-shadow */
 // import { useState } from 'react';
+import Blog from './Components/Modals/Blog.jsx';
 
 function App() {
   return (
+    <div>
     <div className="App">
       Hello World
+    </div>
+    <Blog/>
     </div>
   );
 }
---
+++ Front-End/src/Components/Modals/Blog.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from "react-bootstrap/Modal";
+
+function Blog() {
+  const [modalShow, setModalShow] = React.useState(false);
+  return (
+    <div>
+      <Modal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Blog Post Title
+            <h6>October 1st, 2022</h6>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>
+            Lorem ipsum dolor sit amet, vim reque expetendis eu, est movet sonet
+            necessitatibus ei,nusquam adolescens te vis. An mea exerci eligendi.
+            Deleniti intellegam efficiantur qui ei, hasea reque diceret,
+            summo homero dolores est id. Rebum option perpetua ei mei,
+            solum corporapercipit ne usu.
+          </p>
+          <p>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+            consectetur ac, vestibulum at eros.
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={() => setModalShow(false)}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+      <Button variant="primary" onClick={() => setModalShow(true)}>Blog Post</Button>
+    </div>
+
+  );
+}
+
+export default Blog;
