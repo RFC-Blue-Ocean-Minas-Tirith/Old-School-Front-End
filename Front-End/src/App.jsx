7b4d3ba8f402d7547bbecdfd1b24fab640ca3230
Zachariah1618 <zvallow@gmail.com>
2022-10-03 09:44:32 -0500

    created liveStream branch

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,7 +1,28 @@
 /* eslint-disable no-shadow */
 // import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  // axios.get('http://localhost:8080/user')
+  // .then((res) => console.log(res))
+  // .catch((err) => console.log(err));
+
+  // axios.post('http://localhost:8080/user', {
+  //   params: {
+  //     firstname: 'Zach',
+  //     lastName: 'Vallow',
+  //     profilePicture: 'Default',
+  //     username: 'zvallow',
+  //     email: 'zvallow@gmail.com',
+  //     password: '1234',
+  //     aboutMe: 'I am Zach',
+  //     favCreator: ['Creed']
+  //   }
+  // })
+  // .then((res) => console.log(res))
+  // .catch((err) => console.log(err));
+
+  console.log('HERE');
   return (
     <div className="App">
       Hello World
