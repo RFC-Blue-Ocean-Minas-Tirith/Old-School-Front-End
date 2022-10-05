80c16662b574d303c1e92e05586adf42b031a999
Alice Blank <alicejblank@gmail.com>
2022-10-05 14:32:50 -0500

    Fixed thumbnail resolution

--- Front-End/src/components/ProfilePages/VideoCarouselEntry.jsx
+++ Front-End/src/components/ProfilePages/VideoCarouselEntry.jsx
@@ -4,11 +4,12 @@
 import { Link } from 'react-router-dom';
 
 function VideoCarouselEntry({ video, currentUser }) {
+  let thumb = video.url.replace('.mp4', '.jpg');
   return (
     <div className="video-carousel-video card center-block">
       <div className="card-body">
       <Link to="/video_page" className="nav-link active" aria-current="page" state={{currentUser: currentUser, video: video}}>
-      <img src={video.thumbnail} width="250px" height="250px" style={{ objectFit: 'cover' }} alt="video" className="card-img-top" />
+      <img src={thumb} width="250px" height="250px" style={{ objectFit: 'cover' }} alt="video" className="card-img-top" />
       </Link>
         <div className="card-title text-center">
           {video.title}
