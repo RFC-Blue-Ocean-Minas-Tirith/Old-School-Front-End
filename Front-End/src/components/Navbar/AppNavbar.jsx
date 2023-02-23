04b492384edb132162590fd367f21464d7ee6cdd
melissa-gv <melissagilv@gmail.com>
2023-02-22 18:55:56 -0600

    Enlarges navbar bts and text; add auto margins

--- Front-End/src/components/Navbar/AppNavbar.jsx
+++ Front-End/src/components/Navbar/AppNavbar.jsx
@@ -1,36 +1,37 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Link, useNavigate } from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { signInWithGoogle, signOutGoogle } from './firebase';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Link, useNavigate } from 'react-router-dom'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Form from 'react-bootstrap/Form'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import { signInWithGoogle, signOutGoogle } from './firebase'
 
 function AppNavbar({ setModalShow, isLoggedIn, videoData, setVideoData, currentUser, setFlaggedModalShow }) {
-  const navigate = useNavigate();
-  const [searchTerm, setsearchTerm] = useState('');
-  const [isAdmin, setIsAdmin] = useState('');
-  const [isFiltered, setIsFiltered] = useState(false);
+  const navigate = useNavigate()
+  const [searchTerm, setSearchTerm] = useState('')
+  const [isAdmin, setIsAdmin] = useState('')
+  const [isFiltered, setIsFiltered] = useState(false)
 
   function handleSearchChange(e) {
-    e.preventDefault();
-    setsearchTerm(e.target.value);
+    e.preventDefault()
+    setSearchTerm(e.target.value)
   }
 
-  function handleSearch() {
+  function handleSearch(e) {
+    e.preventDefault()
     if (videoData) {
       axios.get('http://localhost:8080/videos', { params: { searchTerm } })
         .then((response) => {
           console.log('video search results:', response.data)
-          setVideoData(response.data);
-          setIsFiltered(true);
-          navigate('/');
+          setVideoData(response.data)
+          setIsFiltered(true)
+          navigate('/')
         })
         .catch((err) => {
-          console.log('err:', err);
-        });
+          console.log('err:', err)
+        })
     }
   }
 
@@ -43,89 +44,95 @@ function AppNavbar({ setModalShow, isLoggedIn, videoData, setVideoData, currentU
       })
         .then((response) => {
           if (response.data.isAdmin) {
-            setIsAdmin(true);
+            setIsAdmin(true)
           }
-        });
+        })
     }
   }
 
   function resetFilter() {
-    setIsFiltered(false);
-    document.getElementById('searchField').value = '';
+    setIsFiltered(false)
+    document.getElementById('nav-search-field').value = ''
     return axios.get('http://localhost:8080/video')
       .then((res) => {
-        setVideoData(res.data);
-        navigate('/');
-        //console.log(res.data);
+        setVideoData(res.data)
+        navigate('/')
+        //console.log(res.data)
       })
       .catch((err) => {
-        console.log(err);
-      });
+        console.log(err)
+      })
   }
 
   useEffect(() => {
     if (isLoggedIn) {
-      checkIfAdmin();
+      checkIfAdmin()
     }
-  }, [isLoggedIn]);
+  }, [isLoggedIn])
 
   return (
-    <Navbar id="nav" expand="lg">
+    <Navbar id="nav" expand="xl">
       <Container fluid>
-        <Navbar.Brand id="logo" href="/">Old School</Navbar.Brand>
+        <Navbar.Brand id="logo">
+          <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>
+            {' '}
+            Old School
+          </Link>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
+            style={{ maxHeight: '300px' }}
             navbarScroll
           >
-            <Link id="nav-button" to="/" className="nav-link active" aria-current="page">Home</Link>
+            <Link id="nav-button" to="/" className="nav-link" aria-current="page">Home</Link>
 
             {isLoggedIn
               ? <Link id="nav-button" to="profile_page" state={{ currentUser: currentUser, user: currentUser.username }} className="nav-link active" aria-current="page">My Profile</Link>
               : null}
 
-            <Form id="nav-search" className="d-flex">
+            <Form className="d-flex" onSubmit={handleSearch}>
               <Form.Control
-                id="searchField"
+                id="nav-search-field"
                 type="search"
-                placeholder="Search"
-                className="me-2"
+                placeholder="Search videos & users..."
+                className="mx-2"
                 aria-label="Search"
                 onChange={handleSearchChange}
               />
-              <Button id="whiteButton" variant="outline-success" onClick={handleSearch}>Search</Button>
+              <Button id="whiteButton" size="lg" type="submit">Search</Button>
             </Form>
             {isFiltered
               ? (
                 <>
-                  <Navbar.Text style={{ color: 'white', paddingLeft: '10px', paddingRight: '10px' }}>
-                    Search results:&nbsp;
+                  <Navbar.Text id="nav-search-result" className="mx-2">
+                    Results:&nbsp;
                     {videoData.length}&nbsp;
                     {videoData.length === 1 ? 'video' : 'videos'}
                   </Navbar.Text>
-                  <Button type="reset" id="whiteButton" variant="outline-success" onClick={resetFilter}>Clear Search</Button>
+                  <div className="vr" />
+                  <Button size="lg" id="whiteButton" className="mx-2" type="reset" onClick={resetFilter}>Reset</Button>
                 </>
               )
               : null}
           </Nav>
 
           {isLoggedIn && isAdmin
-            ? <Button id="flagged" className="btn btn-primary me-2" type="button" onClick={() => setFlaggedModalShow(true)}>Review Flagged</Button>
+            ? <Button size="lg" id="flagged" className="mx-2" type="button" onClick={() => setFlaggedModalShow(true)}>Review Flagged</Button>
             : null}
 
           {isLoggedIn
-            ? <Button id="whiteButton" className="btn btn-primary me-2" type="button" onClick={() => setModalShow(true)}>Upload</Button>
-            : <Button id="whiteButton" className="btn btn-primary me-2" type="button" onClick={signInWithGoogle}>Upload</Button>}
+            ? <Button size="lg" id="whiteButton" className="mx-2" type="button" onClick={() => setModalShow(true)}>Upload</Button>
+            : <Button size="lg" id="whiteButton" className="mx-2" type="button" onClick={signInWithGoogle}>Upload</Button>}
 
           {isLoggedIn
-            ? <Button id="whiteButton" className="btn btn-primary me-2" type="button" onClick={signOutGoogle}>Logout</Button>
-            : <Button id="whiteButton" className="btn btn-primary me-2" type="button" onClick={signInWithGoogle}>Login</Button>}
+            ? <Button size="lg" id="whiteButton" className="mx-2" type="button" onClick={signOutGoogle}>Logout</Button>
+            : <Button size="lg" id="whiteButton" className="mx-2" type="button" onClick={signInWithGoogle}>Login</Button>}
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  );
+  )
 }
 
-export default AppNavbar;
+export default AppNavbar
